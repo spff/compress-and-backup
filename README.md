0fa4007d43ccdb181c5aaef67494b3fbe7108fdb
spff <spff@bitmessage.ch>
2023-11-19 20:40:54 +0800

    Impl

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # compress-and-backup
+
+The main purpose of this repo is to compress and backup my files to the cloud.
+
+## Considerations
+- Cost
+    - Backup
+    - Store
+        - S3 Standard
+            - 0.023 USD / GB / month.  
+        - S3 Glacier Deep Archive
+            - 0.00099 USD / GB / month, min charge: 180 day, that means if the average data updating frequency is longer than 8 days (180/(0.023/0.00099)), we should consider using S3 Glacier Deep Archive.  
+    - Restore
+        - S3 Standard
+            - Free
+        - S3 Glacier Deep Archive
+            - batch: 0.0025 USD / GB and would need to wait for several hours to begin the restoration.  
+            - Introduce a local HDD backup to decrease the potential to perform a restoration from S3.
+
+- Compression
+    - To decrease S3 cost, we consolidate (Deep Archive: minimum eligible object size of 128 KB) and compress the data.
+    - ZSTD is fast https://peazip.github.io/fast-compression-benchmark-brotli-zstandard.html  
+    - Problems
+        - The external consolidation process merges the files to a new tar file, that would take extra disk space.
+        - zstd.exe doesn't support input/output path containing multi-character words(Japanese).  
+    - Use the https://github.com/mcmilk/7-Zip-zstd instead.
+
+- UX
+    - Easy to use
+    - Shouldn't be error-prone
+    - Write some code in Python, it can do error handling better than bash does.  
+        - Discover
+            - Perform a crc32 checksum of the local files, and compare the values to the metadata of the S3 blobs to discover changes
+        - Backup
+            - If we have an existing zstd-7z archive file, check the crc32 in its metadata with the crc32 of the local files, to see if we need to overwrite the zstd-7z
+            - Do a multipart upload
+            - Add the crc32 checksum of the raw files to the metadata when uploading to the S3
+        - Restore
+            - WON'T IMPLEMENT, restore it manually.
+
+- Diagnose
+    https://aws.amazon.com/tw/blogs/aws-cloud-financial-management/discovering-and-deleting-incomplete-multipart-uploads-to-lower-amazon-s3-costs/
+
+- Ref
+    https://docs.aws.amazon.com/zh_tw/amazonglacier/latest/dev/uploading-an-archive-single-op-using-cli.html
---
+++ main.py
@@ -0,0 +1,222 @@
+import os
+import sys
+import subprocess
+import threading
+import argparse
+import pathlib
+
+import inquirer
+import boto3
+from boto3.s3.transfer import TransferConfig
+from dotenv import load_dotenv
+
+load_dotenv()
+MB = 1024 * 1024
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('base')
+    parser.add_argument('src', help='the relative path from base to be backup')
+    args_config, _ = parser.parse_known_args()
+    return args_config
+
+
+def main():
+    args_config = parse_args()
+    envs = {
+        k: os.getenv(k)
+        for k in [
+            'aws_access_key_id',
+            'aws_secret_access_key',
+            's3_base',
+            '7z_bin',
+        ]
+    }
+    s3 = boto3.session.Session(aws_access_key_id=envs['aws_access_key_id'], aws_secret_access_key=envs['aws_secret_access_key']).resource('s3')
+    bucket_str, prefix = envs['s3_base'].split('/', 1)
+    bucket = s3.Bucket(bucket_str)
+
+    base = pathlib.Path(args_config.base)
+    src = base / args_config.src
+    print(
+        base.absolute().resolve(),
+        src.absolute().resolve(),
+    )
+    subdirs = [x for x in src.iterdir() if x.is_dir()]
+    questions = [
+        inquirer.Checkbox(
+            "interests",
+            message="Select directories to backup",
+            choices=subdirs,
+            default=subdirs,
+        ),
+    ]
+
+    answers = inquirer.prompt(questions)
+    cloud_crcs = {}
+    if not answers or not answers['interests']:
+        print('cancelled')
+        return
+    print(f'looking for {prefix}{args_config.src}')
+    for x in bucket.objects.filter(Prefix=f'{prefix}{args_config.src}', Delimiter='/'):
+        if not x.key.endswith('.7z'):
+            continue
+        cloud_crcs[x.key] = bucket.Object(x.key).metadata.get('checksum-crc32')
+
+    for x in answers['interests']:
+        print(x, x.name)
+        archive_path = x/'..'/f'{x.name}.7z'
+        dir_crc = crc_dir(envs['7z_bin'], x)
+
+        object_key = f'{prefix}{args_config.src}{x.name}.7z'
+
+        if object_key in cloud_crcs:
+            if cloud_crcs[object_key] == dir_crc:
+                print('No changes, skip')
+                continue
+            else:
+                print(f"Found change, dir: {dir_crc}, s3: {cloud_crcs[object_key]}")
+        print(cloud_crcs, object_key)
+        should_compress = True
+        if archive_path.exists():
+            archive_crc = crc_archive(envs['7z_bin'], archive_path)
+            if archive_crc == dir_crc:
+                print('Found archive, upload directly')
+                should_compress = False
+            else:
+                print(f'Found archive but crc not match, dir: {dir_crc}, archive: {archive_crc}, overwrite')
+
+        if should_compress:
+            compress(envs['7z_bin'], archive_path, x)
+            if crc_archive(envs['7z_bin'], archive_path) != dir_crc:
+                raise Exception(
+                    "Something wrong, maybe the dir has been changed after the process started {}, {}".format(
+                        dir_crc,
+                        crc_archive(envs['7z_bin'], archive_path)
+                    )
+                )
+
+        upload(
+            archive_path,
+            bucket,
+            object_key,
+            metadata={'checksum-crc32': dir_crc}
+        )
+        archive_path.unlink()
+
+
+def crc_dir(bin, path):
+    ret = subprocess.run([bin, 'h', '-scrcCRC32', str(path.resolve().absolute())], capture_output=True)
+    for line in ret.stdout.decode('utf-8').replace('\r', '').split('\n'):
+        if line.startswith('CRC32  for data and names:    '):
+            return line.split('CRC32  for data and names:    ')[1]
+    else:
+        raise Exception('Unexpected 7z crc result')
+
+
+def crc_archive(bin, path):
+    ret = subprocess.run([bin, 't', str(path.resolve().absolute()), '-scrcCRC32'], capture_output=True)
+    for line in ret.stdout.decode('utf-8').replace('\r', '').split('\n'):
+        if line.startswith('CRC32  for data and names:    '):
+            return line.split('CRC32  for data and names:    ')[1]
+    else:
+        raise Exception('Unexpected 7z crc result')
+
+
+def compress(bin, archive_path, dir_path):
+    print('Compressing ...')
+    print()
+    ret = subprocess.run([
+        bin,
+        'a',
+        '-m0=zstd',
+        '-mx11',
+        str(archive_path.resolve().absolute()),
+        str(dir_path.resolve().absolute()),
+    ], capture_output=True)
+    if ret.returncode != 0:
+        print()
+        raise Exception(f"Invalid result: {ret.returncode}, {ret.stderr.decode('utf-8')}")
+
+
+class TransferCallback:
+    """
+    Handle callbacks from the transfer manager.
+
+    The transfer manager periodically calls the __call__ method throughout
+    the upload and download process so that it can take action, such as
+    displaying progress to the user and collecting data about the transfer.
+    """
+
+    def __init__(self, target_size):
+        self._target_size = target_size
+        self._total_transferred = 0
+        self._lock = threading.Lock()
+        self.thread_info = {}
+
+    def __call__(self, bytes_transferred):
+        """
+        The callback method that is called by the transfer manager.
+
+        Display progress during file transfer and collect per-thread transfer
+        data. This method can be called by multiple threads, so shared instance
+        data is protected by a thread lock.
+        """
+        thread = threading.current_thread()
+        with self._lock:
+            self._total_transferred += bytes_transferred
+            if thread.ident not in self.thread_info.keys():
+                self.thread_info[thread.ident] = bytes_transferred
+            else:
+                self.thread_info[thread.ident] += bytes_transferred
+
+            target = self._target_size * MB
+            sys.stdout.write(
+                f"\r{self._total_transferred} of {target} transferred "
+                f"({(self._total_transferred / target) * 100:.2f}%)."
+            )
+            sys.stdout.flush()
+
+
+def upload(
+    src,
+    bucket,
+    object_key,
+    metadata=None,
+    storage_class='DEEP_ARCHIVE',
+):
+    '''
+      :param storage_class: 'STANDARD'|'REDUCED_REDUNDANCY'|'STANDARD_IA'|'ONEZONE_IA'|'INTELLIGENT_TIERING'|'GLACIER'|'DEEP_ARCHIVE'|'OUTPOSTS'|'GLACIER_IR'|'SNOW'
+    '''
+    transfer_callback = TransferCallback(src.stat().st_size)
+    config = TransferConfig(multipart_threshold=20 * MB)
+    extra_args = {}
+
+    for k, v in [
+        ('Metadata', metadata),
+        ('StorageClass', storage_class), 
+    ]:
+        if v:
+            extra_args[k] = v
+    print(object_key)
+    bucket.upload_file(
+        src,
+        object_key,
+        Config=config,
+        ExtraArgs=extra_args if extra_args else None,
+        Callback=transfer_callback,
+    )
+    return transfer_callback.thread_info
+
+
+def download(bucket, object_key, download_file_path, file_size_mb):
+    transfer_callback = TransferCallback(file_size_mb)
+    config = TransferConfig(multipart_threshold=20 * MB)
+    bucket.Object(object_key).download_file(
+        download_file_path, Config=config, Callback=transfer_callback
+    )
+    return transfer_callback.thread_info
+
+
+main()
